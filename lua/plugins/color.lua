26a3a971bf8b7b2a59a5929658e0f4e7cf537a6d
Nightmaretopia <absolute_zero0203@icloud.com>
2025-03-12 03:51:10 +0800

    fix colorscheme

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -27,8 +27,12 @@ return {
             },
         },
         config = function ()
+            vim.cmd [[colorscheme catppuccin-mocha]]
+
             vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
             vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+            vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'none' })
+            vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'none' })
         end
     }
 }
