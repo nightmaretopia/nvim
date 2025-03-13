30460fbdfca4de936bb42051d515edc414f2c713
Nightmaretopia <absolute_zero0203@icloud.com>
2025-03-14 04:41:43 +0800

    remove comment remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,5 +3,4 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>rt', function() vim.cmd [[Neotree position=current]] end)
 vim.keymap.set('n', '<leader>ter', '<C-w><C-v><C-w><C-w>:ter<Cr>i')
 vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action)
-vim.keymap.set('n', '<C-/>', 'gc')
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
