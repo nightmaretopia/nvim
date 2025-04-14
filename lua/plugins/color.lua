0d990eed25cd8023ff730f85bd3968451ddb346a
Nightmaretopia <absolute_zero0203@icloud.com>
2025-04-15 02:51:08 +0800

    make NC windows transparent

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -30,6 +30,7 @@ return {
             vim.cmd [[colorscheme catppuccin-mocha]]
 
             vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+            vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
             vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
             vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'none' })
             vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'none' })
