175840600ac79351216c9c2cebbd39785390e8a2
Nightmaretopia <absolute_zero0203@icloud.com>
2025-05-26 14:30:19 +0800

    updated treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     lazy = false,
     build = ':TSUpdate',
     opts = {
         auto_install = true,
-        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'javascript', 'typescript', 'prisma' },
+        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'javascript', 'typescript' },
         indent = { enable = true },
         highlight = { enable = true },
     },
